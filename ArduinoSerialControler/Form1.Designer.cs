f0a1166bda14ab3f30bb8ef6f79759daf335f800
crowmw <crow.mw@gmail.com>
2014-09-05 21:45:08 +0200

    Initialize

---
+++ ArduinoSerialControler/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ArduinoSerialControler
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.MainTextBox = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.EditStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.SendTextBox = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.PortComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(105, 12);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(186, 12);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 4;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // serialPort
+            // 
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // MainTextBox
+            // 
+            this.MainTextBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.MainTextBox.Location = new System.Drawing.Point(12, 42);
+            this.MainTextBox.Multiline = true;
+            this.MainTextBox.Name = "MainTextBox";
+            this.MainTextBox.ReadOnly = true;
+            this.MainTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.MainTextBox.Size = new System.Drawing.Size(249, 172);
+            this.MainTextBox.TabIndex = 5;
+            this.MainTextBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.MainTextBox_MouseDoubleClick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.EditStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 247);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(274, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // EditStatusLabel
+            // 
+            this.EditStatusLabel.Name = "EditStatusLabel";
+            this.EditStatusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // SendTextBox
+            // 
+            this.SendTextBox.Location = new System.Drawing.Point(12, 220);
+            this.SendTextBox.Name = "SendTextBox";
+            this.SendTextBox.Size = new System.Drawing.Size(168, 20);
+            this.SendTextBox.TabIndex = 2;
+            this.SendTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SendTextBox_KeyDown);
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(186, 220);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(75, 23);
+            this.SendButton.TabIndex = 3;
+            this.SendButton.Text = "Wyślij";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // PortComboBox
+            // 
+            this.PortComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.PortComboBox.FormattingEnabled = true;
+            this.PortComboBox.Location = new System.Drawing.Point(13, 13);
+            this.PortComboBox.Name = "PortComboBox";
+            this.PortComboBox.Size = new System.Drawing.Size(86, 21);
+            this.PortComboBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(261, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "?";
+            this.label1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label1_MouseClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(274, 269);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PortComboBox);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.SendTextBox);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.MainTextBox);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "ArduinoSerialControler";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.TextBox MainTextBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel EditStatusLabel;
+        private System.Windows.Forms.TextBox SendTextBox;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.ComboBox PortComboBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ArduinoSerialControler/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoSerialControler
+{
+    public partial class MainForm : Form
+    {
+        string data;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            StartButton.Enabled = false;
+            StopButton.Enabled = false;
+            SendButton.Enabled = false;
+            SendTextBox.Enabled = false;
+            string[] ports = System.IO.Ports.SerialPort.GetPortNames();
+            if (ports.Count() != 0)
+            {
+                StartButton.Enabled = true;
+                foreach (string port in ports)
+                    PortComboBox.Items.Add(port);
+                PortComboBox.SelectedIndex = 0;
+            }
+            else
+                EditStatusLabel.Text = "Brak portów COM";
+
+        }
+
+        private void DisplayData(object sender, EventArgs e)
+        {
+            MainTextBox.AppendText(data);
+            MainTextBox.AppendText(Environment.NewLine);
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            serialPort.Close();
+        }
+
+        private void MainTextBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            MainTextBox.Clear();
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            string data = SendTextBox.Text;
+            serialPort.WriteLine(data);
+            SendTextBox.Clear();
+        }
+
+        private void SendTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                this.Invoke(new EventHandler(SendButton_Click));
+            }
+        }
+
+        private void serialPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            data = serialPort.ReadLine();
+            this.Invoke(new EventHandler(DisplayData));
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if(!serialPort.IsOpen)
+            {
+                try
+                {
+                    serialPort.PortName = PortComboBox.Text;
+                    serialPort.BaudRate = 9600;
+                    serialPort.Open();
+
+                    EditStatusLabel.Text = "Otwarto port " + serialPort.PortName;
+
+                    StartButton.Enabled = false;
+                    SendButton.Enabled = true;
+                    StopButton.Enabled = true;
+                    SendTextBox.Enabled = true;
+                }
+                catch (Exception)
+                {
+                    EditStatusLabel.Text = "Nie można otworzyć portu " + PortComboBox.Text;
+                }
+            }
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            serialPort.Close();
+            EditStatusLabel.Text = "Zamknieto port " + serialPort.PortName;
+
+            StartButton.Enabled = true; 
+            StopButton.Enabled = false;
+            SendButton.Enabled = false;
+        }
+          
+        private void label1_MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("ArduinoSerialControler " + Application.ProductVersion.Substring(0,5) + "\nWykonał crowmw \n\nKomunikacja z Arduino poprzez port COM. \n\nAby komunikować się z Arduino należy wyłączyć monitor portu szeregowego w IDE\n\nDouble-click czyści okno", "Halp!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
